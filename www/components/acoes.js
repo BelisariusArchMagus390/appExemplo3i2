ca07ad9a549bf2e16b4196f122575e451a98523a
BelisariusArchMagus390 <bartsimpsom300@gmail.com>
2019-03-21 02:37:14 +0900

    criação das funções de somar, dividir, multiplicar e subtrair

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -1,2 +1,51 @@
 // This is a JavaScript file
 
+/*$(document).on('click', '#btn7', function(){
+    var valor;
+    valor = $("#display").val();
+    $("#display").val("7");
+});
+*/
+
+var op;
+var valor1;
+
+//inserindo valor no display
+$(document).on('click', '.botao', function(){
+  var valor;
+  valor = $("#display").val();
+  valor += $(this).val();
+  $("#display").val(valor);
+});
+
+//ações dos calculos
+//somar
+$(document).on('click', '#btnSomar', function(){
+  valor1 = $("#display").val();
+  op = "somar";
+  $("#display").val("");
+});
+
+//divisão
+$(document).on('click', '#btnDividir', function(){
+  valor1 = $("#display").val();
+  op = "dividir";
+  $("#display").val("");
+});
+
+//subtração
+$(document).on('click', '#btnSubtrair', function(){
+  valor1 = $("#display").val();
+  op = "subtrair";
+  $("#display").val("");
+});
+
+//multiplicação
+$(document).on('click', '#btnMultiplicar', function(){
+  valor1 = $("#display").val();
+  op = "multiplicar";
+  $("#display").val("");
+});
+
+
+
